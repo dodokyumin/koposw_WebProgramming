34333825d626035387ed3ef18bb5e06570bb52a3
HONGKYUMIN (ANTHONY) <hisp1301@gmail.com>
2022-06-09 15:11:12 +0900

    코드 정렬

--- j/Algo2.java
+++ j/Algo2.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class Algo02 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String conInfo = sc.nextLine();
-		
-		String[] conInfoArr = conInfo.split(" ");
-		double a = Integer.parseInt(conInfoArr[0]);
-		double b = Integer.parseInt(conInfoArr[1]);
-		double R = Integer.parseInt(conInfoArr[2]);
-		double N = sc.nextInt();
-		
-		for (double i = 0; i < N; i++) {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String conInfo = sc.nextLine();
 
-			Scanner sc1 = new Scanner(System.in);
-			String shade = sc1.nextLine();
-			String[] shadeArr = shade.split(" ");
-			double x = Double.parseDouble(shadeArr[0]);
-			double y = Double.parseDouble(shadeArr[1]);
-			
-			if((Math.pow((x-a),2) + Math.pow((y-b),2)) >= Math.pow(R,2)) {
-				System.out.println("silent\n");
-			} else {
-				System.out.println("noisy");
-			}
-		}
-		
-		sc.close();
-	}
+    String[] conInfoArr = conInfo.split(" ");
+    double a = Integer.parseInt(conInfoArr[0]);
+    double b = Integer.parseInt(conInfoArr[1]);
+    double R = Integer.parseInt(conInfoArr[2]);
+    double N = sc.nextInt();
+
+    for (double i = 0; i < N; i++) {
+
+      Scanner sc1 = new Scanner(System.in);
+      String shade = sc1.nextLine();
+      String[] shadeArr = shade.split(" ");
+      double x = Double.parseDouble(shadeArr[0]);
+      double y = Double.parseDouble(shadeArr[1]);
+
+      if ((Math.pow((x - a), 2) + Math.pow((y - b), 2)) >= Math.pow(R, 2)) {
+        System.out.println("silent\n");
+      } else {
+        System.out.println("noisy");
+      }
+    }
+
+    sc.close();
+  }
 
 }
